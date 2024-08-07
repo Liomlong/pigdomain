131783c5ac8591956f6355d5a9f31db8494abbd9
0000 <a1@1deMacBook-Pro.lan>
2024-08-07 11:35:57 +0800

    Fix TON Connect integration

--- client/src/components/TonConnectButton.js
+++ client/src/components/TonConnectButton.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { TonConnectButton, useTonConnect } from '@tonconnect/ui-react';
+import { TonConnectButton } from '@tonconnect/ui-react';
 
 const TonConnect = () => {
-    const { connected, account } = useTonConnect();
-
     return (
         <div>
             <TonConnectButton />
-            {connected && <div>Connected as: {account}</div>}
         </div>
     );
 };
